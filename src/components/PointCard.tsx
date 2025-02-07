e65bee595b382c545f5efc3d680c773482c53d4f
Maure Andrade <pedro.maure@gmail.com>
2025-02-06 23:40:16 -0300

    style: refactor of the appearence of the point card

--- src/components/PointCard.tsx
+++ src/components/PointCard.tsx
@@ -11,27 +11,25 @@ export const PointCard = ({
   onDelete,
   onEdit,
 }: PointCardProps): JSX.Element => {
-  const { id, name, description, value, badges } = point;
+  const { name, description, value, badges } = point;
 
   return (
-    <li className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-      <strong className="text-lg font-medium text-gray-900">{name}</strong>
-      <p className="text-sm text-gray-700 mt-1">{description}</p>
-      <p className="text-sm text-gray-700 mt-1">Valor: R$ {value}</p>
-      <p className="text-sm text-gray-700 mt-1">Badges: {badges.join(", ")}</p>
-      <div className="mt-4 space-x-2">
-        <button
-          onClick={() => onEdit(point)}
-          className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        >
-          Editar
-        </button>
-        <button
-          onClick={() => onDelete(id)}
-          className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-        >
-          Excluir
-        </button>
+    <li className="bg-white py-5 px-4 rounded-[15px] gap-2 border border-gray-300">
+      {/* Título, descrição e dropdown */}
+      <div className="flex flex-col gap-0">
+        <div className="flex justify-between">
+          <h2 className="text-lg font-medium text-gray-900">{name}</h2>
+          <i className="fa-solid fa-ellipsis" />
+        </div>
+        <p className="text-xs text-normal text-gray-500">{description}</p>
+      </div>
+
+      {/* Valor e badges */}
+      <div className="flex flex-col">
+        <p className="text-sm text-emerald-800">
+          <i className="fa-solid fa-coins" /> R$ {value}
+        </p>
+        <p className="text-sm text-gray-500">{badges.join(" • ")}</p>
       </div>
     </li>
   );
