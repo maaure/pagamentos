b53fbdef39f9617b141c034079b626a91442a129
Maure Andrade <pedro.maure@gmail.com>
2025-02-08 14:25:51 -0300

    feat: enhance input component with error handling and validation messages

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,32 @@
 import { forwardRef } from "react";
+import { FieldErrors } from "react-hook-form";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  errors?: FieldErrors;
 }
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, ...props }, ref) => {
+  ({ label, errors, ...props }, ref) => {
+    const hasErrors = props.name && errors && errors[props.name];
+
     return (
       <>
         <div>
-          <label className="relative text-sm text-gray-700 font-medium block pb-2">
+          <label
+            className={`relative text-sm text-gray-700 font-medium block pb-2  ${hasErrors ? "text-red-500" : ""}`}
+          >
             {label}
           </label>
-          <input className="input-rounded w-full" {...props} ref={ref} />
+          <input
+            className={`input-rounded w-full ${hasErrors && "error"}`}
+            {...props}
+            ref={ref}
+          />
+          {hasErrors && props.name && (
+            <span className="text-red-500 text-sm">
+              {errors[props.name]?.message?.toString() || ""}
+            </span>
+          )}
         </div>
       </>
     );
