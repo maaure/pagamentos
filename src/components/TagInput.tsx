59fa1bbd3aaa0a446ff5b9e20644f38140e8c8c4
Maure Andrade <pedro.maure@gmail.com>
2025-02-09 15:29:15 -0300

    fix: update TagInput component to correctly handle value prop and improve label interaction

--- src/components/TagInput.tsx
+++ src/components/TagInput.tsx
@@ -5,13 +5,13 @@ interface TagInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   defaultValue?: string;
 }
 
+/* Known Issue: Click on label dont focus the field */
+
 export const TagInput = forwardRef<HTMLInputElement, TagInputProps>(
-  ({ label, placeholder, onChange, ...props }, ref) => {
+  ({ label, placeholder, onChange, value, ...props }, ref) => {
     const [inputValue, setInputValue] = useState<string>("");
     const [tags, setTags] = useState<string[]>(
-      props.value
-        ? (props.value as string).split(",").map((s) => s.trim())
-        : [],
+      value ? (value as string).split(",").map((s) => s.trim()) : [],
     );
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -39,36 +39,36 @@ export const TagInput = forwardRef<HTMLInputElement, TagInputProps>(
           htmlFor={props.name}
         >
           {label}
-        </label>
-        <div className="input relative">
-          <div className="flex flex-wrap gap-2">
-            {tags.map((tag, index) => (
-              <div
-                key={`${tag}-${index}`}
-                className="flex items-center bg-gray-100 border border-gray-300 rounded-lg px-2 py-1 text-sm"
-              >
-                {tag}
-              </div>
-            ))}
-            <input
-              type="text"
-              className="hidden"
-              id="tags"
-              onChange={onChange}
-              value={tags.join(",")}
-              ref={ref}
-              {...props}
-            />
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyDown}
-              className="flex-1 min-w-[50px] outline-none bg-transparent"
-              placeholder={placeholder}
-            />
+          <div className="input relative">
+            <div className="flex flex-wrap gap-2">
+              {tags.map((tag, index) => (
+                <div
+                  key={`${tag}-${index}`}
+                  className="flex items-center bg-gray-100 border border-gray-300 rounded-lg px-2 py-1 text-sm"
+                >
+                  {tag}
+                </div>
+              ))}
+              <input
+                type="text"
+                className="hidden"
+                id="tags"
+                onChange={onChange}
+                value={tags.join(",")}
+                ref={ref}
+              />
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                className="flex-1 min-w-[50px] outline-none bg-transparent"
+                placeholder={placeholder}
+                {...props}
+              />
+            </div>
           </div>
-        </div>
+        </label>
       </>
     );
   },
